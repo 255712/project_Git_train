ec73544e3c3341452f41d29a97e51ad41600f5db
student <255712@edu.p.lodz.pl>
2024-10-25 09:38:13 +0200

    zmiana mnożenia w metodzie Policz

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ int main(void) {
     int a=2;
     int b=3;
     printf("Hello, World!\n");
-    printf("suma liczb 2 oraz 3 to %d\n" ,a+b);
+    printf("suma liczb 2 oraz 3 to %d\n" ,a*b);
     return 0;
 }
